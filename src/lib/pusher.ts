02a68336ff59c16d0ca28208414d5311a332dffe
Refren86 <denis1975shvaiber@gmail.com>
2023-11-12 15:51:43 +0200

    test

--- src/lib/pusher.ts
+++ src/lib/pusher.ts
@@ -1,10 +1,32 @@
 import PusherServer from 'pusher';
 import PusherClient from 'pusher-js';
 
+function getPusherCredentials() {
+  const appId = process.env.PUSHER_APP_ID;
+  const key = process.env.NEXT_PUBLIC_PUSHER_APP_KEY;
+  const secret = process.env.PUSHER_APP_SECRET;
+
+  if (!appId) {
+    throw new Error('Missing pusher appId');
+  }
+
+  if (!key) {
+    throw new Error('Missing pusher key');
+  }
+
+  if (!secret) {
+    throw new Error('Missing pusher secret');
+  }
+
+  return {
+    appId,
+    key,
+    secret,
+  };
+}
+
 export const pusherServer = new PusherServer({
-  appId: process.env.PUSHER_APP_ID!,
-  key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
-  secret: process.env.PUSHER_APP_SECRET!,
+  ...getPusherCredentials(),
   cluster: 'eu',
   useTLS: true, // encrypted data traffic (probably)
 });
