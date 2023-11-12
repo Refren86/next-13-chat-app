daf261b0da88eac5e00d800a15fe8cd6b4eb57ad
Refren86 <denis1975shvaiber@gmail.com>
2023-11-12 16:25:20 +0200

    removed comment

--- src/types/db.d.ts
+++ src/types/db.d.ts
@@ -3,14 +3,6 @@ type Chat = {
   messages: Message[];
 }
 
-// type Message = {
-//   id: string;
-//   senderId: string;
-//   receiverId: string;
-//   text: string;
-//   timestamp: number;
-// }
-
 type FriendRequest = {
   id: string;
   senderId: string;
